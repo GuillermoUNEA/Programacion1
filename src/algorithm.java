78eb09c607da5fa47549ffed71cde0cb98727e37
Guillermo Mart�n Ch�vez Egas <guillermo.chavez@unealum.local>
2025-01-17 13:28:01 +0100

    funcional con identificacion y numeros

--- src/algorithm.java
+++ src/algorithm.java
@@ -3,7 +3,7 @@ class algorithm{
     public static void main(String[] args) {
         int evenNumbers=0;
         int oddNumbers=0;
-        int allnumbers=0;
+        int allNumbers=0;
         int newOdds,newEvens,newAll;
         Scanner userID=new Scanner(System.in);
         System.out.println("Enter ID: ");
@@ -17,7 +17,7 @@ class algorithm{
             i=i+1;
         }
         for(int j=(intArrayID.length-1);j>=0;j--){
-            allnumbers=allnumbers+intArrayID[j];
+            allNumbers=allNumbers+intArrayID[j];
             if(j%2==1){
                 oddNumbers=oddNumbers+intArrayID[j];
             }
@@ -26,24 +26,53 @@ class algorithm{
             }    
         }
 
-        System.out.println("Odd Number Sum="+oddNumbers);
         System.out.println("Even Number Sum="+evenNumbers);
-        System.out.println("All Number Sum="+allnumbers);
+        System.out.println("Odd Number Sum="+oddNumbers);
+        System.out.println("All Number Sum="+allNumbers);
         newOdds=oddNumbers+1;
         newEvens=evenNumbers+1;
-        newAll=allnumbers+1;
+        newAll=allNumbers+1;
 
-        int questionOne=theGenerator(oddNumbers);
-        System.out.println(questionOne);
+        int questionOne=theGenerator(newEvens);
+        int questionTwo=theGenerator(newOdds);
+        int questionThree=theGenerator(newAll);
+
+        while(questionTwo==questionOne || questionTwo==questionThree || questionThree==questionOne){
+            if(questionTwo==questionOne && questionTwo==6){
+                questionTwo=1;
+            }
+            if (questionTwo==questionOne){
+                questionTwo++;
+            }
+            if(questionTwo==questionThree && questionTwo==6){
+                questionTwo=1;
+            }
+            if (questionTwo==questionThree) {
+                questionTwo++;
+            }
+            if(questionThree==questionOne && questionThree==6){
+                questionThree=1;
+            }
+            if (questionThree==questionOne){
+                questionThree++;
+            }
+
+        
+        System.out.println("Question One:"+questionOne);
+        System.out.println("Question Two:"+questionTwo);
+        System.out.println("Question Three:"+questionThree);
+        } 
     }
 
         
         static int theGenerator(int theNumber) {
+            if(theNumber/6==0){
+                return 6;}
             if(theNumber>6){
                 int division=theNumber/6;
                 theNumber=theNumber-6*division;
                 //System.out.println("dude was bigger than 6, is now "+theNumber);
-                return theNumber;
             }
+            return theNumber;
         }
 } 
