58b19818072897080369f694ab28f519e2cb708a
Guillermo Mart�n Ch�vez Egas <guillermo.chavez@unealum.local>
2025-01-17 12:35:24 +0100

    funcionalidad en display impresion

--- src/algorithm.java
+++ src/algorithm.java
@@ -1,11 +1,13 @@
+import java.util.Scanner;
 class algorithm{
     public static void main(String[] args) {
         int evenNumbers=0;
         int oddNumbers=0;
         int allnumbers=0;
-        //Scanner userID=new Scanner(System.in);
-        //String ID=userID.nextLine();
-        String ID="1234";
+        Scanner userID=new Scanner(System.in);
+        System.out.println("Enter ID: ");
+        String ID=userID.nextLine();
+        
         char charArrayID[]=ID.toCharArray();
         int intArrayID[]=new int[charArrayID.length];
 
@@ -15,10 +17,21 @@ class algorithm{
         }
         for(int j=(intArrayID.length-1);j>=0;j--){
             allnumbers=allnumbers+intArrayID[j];
-            System.out.println(j%2);
-            // System.out.println(allnumbers);
-
+            if(j%2==1){
+                oddNumbers=oddNumbers+intArrayID[j];
+            }
+            if(j%2==0){
+                evenNumbers=evenNumbers+intArrayID[j];
+            }    
         }
+//        System.out.print("your ID: ");
+//        for(int n=0;n>=intArrayID.length;n++){
+//            System.out.print(intArrayID[n]);
+//        }
+//        System.out.println();
 
+        System.out.println("Odd Number Sum="+oddNumbers);
+        System.out.println("Even Number Sum="+evenNumbers);
+        System.out.println("All Number Sum="+allnumbers);
     }
 } 
