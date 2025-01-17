0389e927e14b77081d87bd4e23aa25840fc3ce4b
GuillermoUNEA <guillermo.chavez@alumnos.uneatlantico.es>
2025-01-17 11:48:21 +0100

    initial documents start

---
+++ src/algorithm.java
@@ -0,0 +1,8 @@
+import  java.util.Scanner;
+class algorithm{
+    public static void main(String[] args) {
+        int evenNumbers=0;
+        int oddNumbers=0;
+        Scanner userID=new Scanner(System.in);
+    }
+} 
