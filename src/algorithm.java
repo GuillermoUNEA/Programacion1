c5cbece55cdfd6e4e7d95cb87b9780267c2ff777
GuillermoUNEA <guillermo.chavez@alumnos.uneatlantico.es>
2025-01-17 12:01:15 +0100

    string to array se caracteres

--- src/algorithm.java
+++ src/algorithm.java
@@ -1,12 +1,15 @@
-import  java.util.Scanner;
 class algorithm{
     public static void main(String[] args) {
         int evenNumbers=0;
         int oddNumbers=0;
-        Scanner userID=new Scanner(System.in);
-        String ID=userID.nextLine();
-        
-
+        //Scanner userID=new Scanner(System.in);
+        //String ID=userID.nextLine();
+        String ID="1234";
+        char arrayID[]=ID.toCharArray();
+        for(int i=0;i<arrayID.length;){
+            System.out.println(arrayID[i]);
+            i=i+1;
+        }
 
     }
 } 
